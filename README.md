9d138efbb2e31b8789a402cbfb848a60c0fc481b
Bart Prokop <bart@prokop.dev>
2021-05-03 02:19:35 +0100

    Adding HMAC SHA512

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 Maven coordinates:
 # My version of Java "swiss army knife" - all utilities I need, without cluttering classpath...
 
+## Goals
+
+The following are important to me:
+ - No checked exceptions - only RuntimeExceptions allowed, all from ```java.lang.*``` package.
+
 ```xml
 <dependency>
     <groupId>dev.prokop.commons</groupId>
     <artifactId>java-army-knife</artifactId>
-    <version>1.0.1</version>
+    <version>1.0.2</version>
 </dependency>
 ```
--- src/main/java/dev/prokop/utils/HashUtils.java
+++ src/main/java/dev/prokop/utils/HashUtils.java
@@ -1,5 +1,8 @@
 package dev.prokop.utils;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -8,12 +11,65 @@ public class HashUtils {
     private HashUtils() {
     }
 
-    public static byte[] sha256(byte[] bytes) {
+    public static MessageDigest sha256() {
         try {
-            return MessageDigest.getInstance("SHA-256").digest(bytes);
+            return MessageDigest.getInstance("SHA-256");
         } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
-            throw new AssertionError("It strange but JVM does not have SHA-256 implementation...", noSuchAlgorithmException);
+            throw new AssertionError("It strange, but JVM does not have SHA-256 implementation...", noSuchAlgorithmException);
         }
+    }
+
+    public static MessageDigest sha512() {
+        try {
+            return MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+            throw new AssertionError("It strange, but JVM does not have SHA-512 implementation...", noSuchAlgorithmException);
+        }
+    }
+
+    public static Mac hmacSha512(byte[] key) {
+        if (key == null) throw new IllegalArgumentException("HMAC key cannot be null reference");
+        try {
+            final Mac mac = Mac.getInstance("HmacSHA512");
+            final SecretKeySpec secretKeySpec = new SecretKeySpec(key, "HmacSHA512");
+            mac.init(secretKeySpec);
+            return mac;
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssertionError("It strange, but JVM does not have HMAC with SHA-512 implementation...", e);
+        } catch (InvalidKeyException invalidKeyException) {
+            throw new IllegalArgumentException("Unable to initialize HMAC with provided key", invalidKeyException);
+        }
+    }
+
+    /**
+     * SHA-256
+     *
+     * @param bytes input
+     * @return sha256(input)
+     */
+    public static byte[] sha256(byte[] bytes) {
+            return sha256().digest(bytes);
+    }
+
+    /**
+     * SHA-512
+     *
+     * @param bytes input
+     * @return sha512(input)
+     */
+    public static byte[] sha512(byte[] bytes) {
+            return sha512().digest(bytes);
+    }
+
+    /**
+     * HMAC SHA-512
+     *
+     * @param key HMAC secret key
+     * @param bytes input
+     * @return hmacSha512(secret_key, input)
+     */
+    public static byte[] hmacSha512(byte[] key, byte[] bytes) {
+        return hmacSha512(key).doFinal(bytes);
     }
 
 }
